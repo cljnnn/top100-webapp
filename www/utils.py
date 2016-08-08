dba83535dd277b48d78d6f2ca28317b6fff4e1e2
Linjun Chen <linjun@linjun.win>
2016-08-08 11:20:35 +0800

    add ConnectionContextManager

---
+++ www/utils.py
@@ -0,0 +1,16 @@
+class ConnectionContextManager(object):
+
+    def __init__(self, engine):
+        self.conn = None
+        self.engine = engine
+
+    async def __aenter__(self):
+        self.conn = await self.engine.acquire()
+        return self.conn
+
+    async def __aexit__(self, exc_type, exc, tb):
+        try:
+            self.engine.release(self.conn)
+        finally:
+            self.conn = None
+            self.engine = None
--- www/views.py
+++ www/views.py
@@ -1,16 +1,17 @@
 import asyncio, aiohttp_jinja2
 from aiohttp import web
 from storage import update_table
+from utils import ConnectionContextManager
 
 @aiohttp_jinja2.template('index.html')
 def index(request):
     return {'is_index': True}
 
 @aiohttp_jinja2.template('list.html')
-def db_update(request):
-    with (yield from request.app['db']) as conn:
+async def db_update(request):
+    async with ConnectionContextManager(request.app['db']) as conn:
         query = update_table.select().order_by(update_table.c.id.desc()).limit(20)
-        update_list = yield from conn.execute(query)
+        update_list = await conn.execute(query)
 
     if not update_list:
         raise web.HTTPNotFound()
